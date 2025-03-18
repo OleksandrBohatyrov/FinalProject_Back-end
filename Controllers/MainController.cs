10f340a4d2f650b728a9b1459635cc039041d58a
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2025-03-18 12:18:30 +0000

    Main controller created

---
+++ Controllers/MainController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject_Back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+       
+    }
+}
